89d70a4788ab6888778eb38b5b4eafc7c0fa809f
wulex <945898393@qq.com>
2021-08-04 01:03:10 +0800

    初始化项目

---
+++ PDFToImgCoreDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using GroupDocs.Conversion;
+using GroupDocs.Conversion.Contracts;
+using GroupDocs.Conversion.FileTypes;
+using GroupDocs.Conversion.Options.Convert;
+
+namespace PDFToImgCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fileName = "Convention-de-stage.pdf";
+            ConvertPdfToImage(fileName, ImageFileType.Jpg);
+        }
+        private static void ConvertPdfToImage(string fileName, ImageFileType  outputFileType)
+        {
+            String sourcePath = @"D:\";
+            String targetPath = @"D:\";
+
+            
+            string outputFileTemplate = Path.Combine(targetPath, "converted-page-{0}.jpg");
+            SavePageStream getPageStream = page => new FileStream(string.Format(outputFileTemplate, page), FileMode.Create);
+            using (Converter converter = new Converter(sourcePath + fileName))
+            {
+                ImageConvertOptions options = new ImageConvertOptions
+                {
+                    Format = ImageFileType.Jpg
+                };
+
+                converter.Convert(getPageStream, options);
+            }
+
+        }
+    }
+}
